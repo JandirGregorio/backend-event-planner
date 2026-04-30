87eaa3383fd9c64b843884e0e63dd8cbc78fc21a
Jandir <jandirgregorio@icloud.com>
2026-04-30 07:37:50 -0400

    feat: add user controllers

---
+++ server/controllers/userControllers.js
@@ -0,0 +1,40 @@
+const userModel = require('../models/userModel');
+
+// PATCH /api/users/:user_id
+module.exports.updateUser = async (req, res, next) => {
+  try {
+    const userId = Number(req.params.user_id);
+
+    if (userId !== req.session.userId) {
+      return res.status(403).send({ message: 'You can only update your own account.' });
+    }
+
+    const { password } = req.body;
+    const user = await userModel.updateUser(userId, password);
+    if (!user) {
+      return res.status(404).send({ message: 'User not found'} );
+    }
+    res.send(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// DELETE /api/users/:user_id
+module.exports.deleteUser = async (req, res, next) => {
+  try {
+    const userId = Number(req.params.user_id);
+
+    if (userId !== req.session.userId) {
+      return res.status(403).send({ message: 'You can only delete your own account'} );
+    }
+
+    const user = await userModel.destroyUser(userId);
+    if (!user) {
+      return res.status(404).send({ message: 'User not found' });
+    }
+    res.send(user);
+  } catch (err) {
+    next(err);
+  }
+};
